4c071a01988b67c7f2ab41307c33009087f8eb13
Hacker Y2J <60312092+HACK3RY2J@users.noreply.github.com>
2020-03-19 12:47:06 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,4 +2,3 @@
 command -v curl > /dev/null 2>&1 || {  echo >&2 "It require curl, but it is not installed. \n installing it.."; apt install curl -y; }
 command -v python > /dev/null 2>&1 || {  echo >&2 "It require python, but it is not installed. \n installing it.."; apt install python -y; }
 command -v pip > /dev/null 2>&1 || {  echo >&2 "It require pip, but it is not installed. \n installing it.."; curl https:bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py ; }
-command -v toilet > /dev/null 2>&1 || {  echo >&2 "It require toilet package but it is not installed. \n installing it.."; apt install toilet -y; }
