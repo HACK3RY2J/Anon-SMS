e271aa6b0a968d2971a84700ac48b13a1f8cd31f
Hacker Y2J <60312092+HACK3RY2J@users.noreply.github.com>
2020-04-14 19:24:51 +0530

    Removing The Useless Stuffs

--- requirements.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-command -v curl > /dev/null 2>&1 || {  echo >&2 "It require curl, but it is not installed. \n installing it.."; apt install curl -y; }
-command -v python > /dev/null 2>&1 || {  echo >&2 "It require python, but it is not installed. \n installing it.."; apt install python -y; }
-command -v pip > /dev/null 2>&1 || {  echo >&2 "It require pip, but it is not installed. \n installing it.."; curl https:bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py ; }
