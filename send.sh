5f66c54a4211072499f5dc792c3b89deabe7da6f
Brennan Mccown <clu3bot@protonmail.com>
2021-07-12 19:42:12 -0400

    Update send.sh

--- send.sh
+++ send.sh
@@ -2,13 +2,13 @@
 # Author: HACK3RY2J
 # Description: Send anonymous text messages
 
-# Colors
 
 # Reset
 NC='\033[0m'       # Text Reset
 
 BLINK=$(tput blink)
 
+#Colors
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
 Yellow='\033[0;33m'       # Yellow
@@ -19,7 +19,7 @@ BGreen='\033[1;32m'       # Green
 
 
 function banner() {
-   
+
    clear
    echo -e "     ████████████████████████████████████████████████████████████████████
      ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████
@@ -36,108 +36,99 @@ function banner() {
      ██                                                                ██
      ████████████████████████████████████████████████████████████████████
      ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ "
-   
-   echo " "
-   echo -e "$BGreen Hello User... $NC"
+
+   echo -e "\n$BGreen Hello User... $NC"
    echo -e "$Green   Welcome to Anon-SMS $NC"
    echo -e "$Green     With Anon-SMS You can send, Anonymous Text Message $NC"
    sleep 1
-   echo -e "$Green       Let's begin... $NC"
-   echo " "
+   echo -e "$Green       Let's begin... $NC\n"
    sleep 2
    read -n 1 -s -r -p "     Press any key to continue"
 }
 
 function byemsg() {
-   
-   
+
    echo -e "$Green Done... $NC"
    sleep 1
    echo -e "$Green Closing Anon-SMS... $NC"
    sleep 1
-   echo " "
-   echo -e "$BGreen Bye User Lets Meet Next Time... $NC"
-   echo " "
+   echo -e "\n$BGreen Bye User Lets Meet Next Time... $NC\n"
    exit
 }
 
 function VERSION() {
-   echo " "
-   echo -e "$BGreen Anon-SMS version: v1.4(Non-Supported)"
-   echo " "
+echo -e "\n$BGreen Anon-SMS version: v1.4(Non-Supported)\n"
 }
 
 function SENDSMS() {
-    echo ""
-   echo -e "$Red      Enter Phone Number Like Country Code>Number"
-   echo -e "$Yellow      For Example country code = $Blue 91 $Green And Phone Number = $Blue 5566778899"
-   echo -e "$Yellow      Enter Number as > $Blue 915566778899"
-   echo -e "$Green      Enter Phone Number With Country Code: $NC"
-   
-   read PHONE
-   
-   echo "  "
-   echo -e "$Green Enter Message: $NC"
-   
-   read SMS
+        clear
+   echo -e "     ████████████████████████████████████████████████████████████████████
+     ████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████
+     ██▒▒██         ___   _  ______  _  __    ______  _______      ██▒▒██
+     ██  ▒▒██      / _ | / |/ / __ \/ |/ /___/ __/  |/  / __/    ██▒▒  ██
+     ██    ▒▒██   / __ |/    / /_/ /    /___/\ \/ /|_/ /\ \    ██▒▒    ██
+     ██      ▒▒██/_/ |_/_/|_/\____/_/|_/   /___/_/  /_/___/  ██▒▒      ██
+     ██        ▒▒██                                    V1.4██▒▒        ██
+     ██          ▒▒████████████████████████████████████████▒▒          ██
+     ██            ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒            ██
+     ██     Coded by: https://github.com/HACK3RY2J                     ██
+     ██     Youtube : https://www.youtube.com/c/PandaHackers           ██
+     ██   Instagram : https://instagram.com/Panda_Hackers_Official     ██
+     ██                                                                ██
+     ████████████████████████████████████████████████████████████████████
+     ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ "
 
+   echo -e "\n$Red Enter Phone Number Like Country Code>Number"
+   echo -e "$Yellow For Example country code = $Blue 91 $Green And Phone Number = $Blue 5566778899"
+   echo -e "$Yellow Enter Number as > $Blue 915566778899"
+   echo -e "$Green Enter Phone Number With Country Code: $NC"
 
-   SMSRESULT=$(curl -# -X POST https://textbelt.com/text --data-urlencode phone="$PHONE" --data-urlencode message="$SMS" -d key=textbelt)
-   
+   read -r PHONE
+   echo -e "\n$Green Enter Message: $NC"
+
+   read -r SMS
+
+SMSRESULT=$(curl -# -X POST https://textbelt.com/text --data-urlencode phone="$PHONE" --data-urlencode message="$SMS" -d key=textbelt)   
    if grep -q true <<<"$SMSRESULT"
-   
+
    then
-      
-      echo "  "
-      echo -e "$BGreen SUCCESS $NC"
-      echo "  "
+
+
+      echo -e "\n$BGreen SUCCESS $NC\n"
       echo -e "$BGreen ---------------------------------------------- $NC"
       echo "$SMSRESULT"
-      echo -e "$BGreen ---------------------------------------------- $NC"
-      echo "  "
-      byemsg
+      echo -e "$BGreen ---------------------------------------------- $NC\n"
+     byemsg
    else
-      
-      echo "  "
-      echo -e "$BRed FAIL $NC"
-      echo "  "
+
+      echo -e "\n$BRed FAIL $NC\n"
       echo -e "$BGreen ---------------------------------------------- $NC"
       echo "$SMSRESULT"
-      echo -e "$BGreen ---------------------------------------------- $NC"
-      echo " "
+      echo -e "$BGreen ---------------------------------------------- $NC\n"
       byemsg
    fi
 }
 
 function SMSCHECK() {
-   
-   echo "  "
-   echo -e "$Green Enter Text ID (example 12589): $NC"
-   
-   read TEXTID
-   
+
+   echo -e "\n$Green Enter Text ID (example 12589): $NC"
+read TEXTID
+
    STATUSRESULT=$(curl -# https://textbelt.com/status/"$TEXTID")
-   
-   echo " "
-   echo -e "$BGreen Final Response (JSON): $NC"
-   echo " "
-   echo -e "$BGreen ---------------------------------------------- $NC"
+
+   echo -e "\n$BGreen Final Response (JSON): $NC"
+   echo -e "\n$BGreen ---------------------------------------------- $NC"
    echo "$STATUSRESULT"
-   echo -e "$BGreen ---------------------------------------------- $NC"
-   echo "  "
+   echo -e "$BGreen ---------------------------------------------- $NC\n"
 }
 
 function helpfunction() {
-   
-   echo "  "
-   echo -e "$BGreen Available opions: $NC"
-   echo -e " "
-   echo -e "$BGreen Show this page: --help $NC"
+
+   echo -e "\n$BGreen Available opions: $NC"
+   echo -e "\n$BGreen Show this page: --help $NC"
    echo -e "$BGreen Check text message status: --statuscheck $NC"
    echo -e "$BGreen Send text message: --sendsms $NC"
-   echo -e "$BGreen Print version: --version $NC"
-   echo "  "
-   echo "  "
+   echo -e "$BGreen Print version: --version $NC\n"
 }
 
 if [ "$1" == "--statuscheck" ]
@@ -158,9 +149,9 @@ elif [ "$1" == "--help" ]
 
 then
    helpfunction
-   
+
 elif [ "$1" == "--version" ]
-   
+
    then
       VERSION
 
@@ -168,15 +159,11 @@ elif [ "$1" == "--version" ]
 elif [ $# -le 0 ]
 
 then
-   echo " "
-   echo -e "$Red No arguments specified! $NC"
-   echo -e "$Red Use$NC $BRed--help$NC $Red to display options.$NC"
-   echo " "
+   echo -e "\n$Red No arguments specified! $NC"
+   echo -e "$Red Use$NC $BRed--help$NC $Red to display options.$NC\n"
 
 else
-   echo " "
-   echo -e "$Red No such argument available!$NC"
-   echo -e "$Red Use$NC $BRed--help$NC $Red to display options.$NC"
-   echo " "
+   echo -e "\n$Red No such argument available!$NC"
+   echo -e "$Red Use$NC $BRed--help$NC $Red to display options.$NC\n"
 
 fi
