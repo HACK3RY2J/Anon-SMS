7db68733e6e5d9464b595971e1f2b409b2011bd2
Hacker Y2J <60312092+HACK3RY2J@users.noreply.github.com>
2024-01-10 20:46:54 +0530

    Update send.sh

--- send.sh
+++ send.sh
@@ -2,13 +2,9 @@
 # Author: HACK3RY2J
 # Description: Send anonymous text messages
 
-# Colors
-
-# Reset
-NC='\033[0m'       # Text Reset
-
+# Text Formatting 
+NC='\033[0m'              # RESET
 BLINK=$(tput blink)
-
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
 Yellow='\033[0;33m'       # Yellow
@@ -16,6 +12,7 @@ Blue='\033[0;34m'         # Blue
 Purple='\033[0;35m'       # Purple
 BRed='\033[1;31m'         # Red
 BGreen='\033[1;32m'       # Green
+
 
 
 function banner() {
@@ -49,21 +46,17 @@ function banner() {
 }
 
 function byemsg() {
-   
-   
    echo -e "$Green Done... $NC"
    sleep 1
    echo -e "$Green Closing Anon-SMS... $NC"
    sleep 1
-   echo " "
-   echo -e "$BGreen Bye User Lets Meet Next Time... $NC"
    echo " "
    exit
 }
 
 function VERSION() {
    echo " "
-   echo -e "$BGreen Anon-SMS version: v1.4(Non-Supported)"
+   echo -e "$BGreen Anon-SMS version: v1.5(Basic)"
    echo " "
 }
 
@@ -73,53 +66,39 @@ function SENDSMS() {
    echo -e "$Yellow      For Example country code = $Blue 91 $Green And Phone Number = $Blue 5566778899"
    echo -e "$Yellow      Enter Number as > $Blue 915566778899"
    echo -e "$Green      Enter Phone Number With Country Code: $NC"
-   
    read PHONE
    
-   echo "  "
-   echo -e "$Green Enter Message: $NC"
-   
+   echo -e "$Green \nEnter Message: $NC"
    read SMS
 
-
-   SMSRESULT=$(curl -# -X POST https://textbelt.com/text --data-urlencode phone="$PHONE" --data-urlencode message="$SMS" -d key=textbelt)
-   
-   if grep -q true <<<"$SMSRESULT"
-   
-   then
-      
-      echo "  "
-      echo -e "$BGreen SUCCESS $NC"
-      echo "  "
-      echo -e "$BGreen ---------------------------------------------- $NC"
-      echo "$SMSRESULT"
-      echo -e "$BGreen ---------------------------------------------- $NC"
-      echo "  "
-      byemsg
+   API_FILE=".Apis"
+   if [ -e "$API_FILE" ]; then
+      API=$(head -n 1 "$API_FILE")
+      echo "API Key : $API"
    else
-      
-      echo "  "
-      echo -e "$BRed FAIL $NC"
-      echo "  "
-      echo -e "$BGreen ---------------------------------------------- $NC"
-      echo "$SMSRESULT"
-      echo -e "$BGreen ---------------------------------------------- $NC"
-      echo " "
-      byemsg
+      API='textbelt'
+      echo "Using Default API : $API"
    fi
+
+   SMSRESULT=$(curl -# -X POST https://textbelt.com/text --data-urlencode phone="$PHONE" --data-urlencode message="$SMS" -d key="$API")
+   
+   if grep -q true <<<"$SMSRESULT"; then
+        echo -e "\n${BGreen}  SUCCESS  ${NC}"
+    else
+        echo -e "\n${BRed}  FAILED  ${NC}"
+    fi
+
+    echo -e "\n${Yellow}----------------------------------------------${NC}"
+    echo "$SMSRESULT"
+    echo -e "${Yellow}----------------------------------------------${NC}\n"
+    byemsg
 }
 
 function SMSCHECK() {
-   
-   echo "  "
-   echo -e "$Green Enter Text ID (example 12589): $NC"
-   
-   read TEXTID
-   
+   echo -e "\n$Green Enter Text ID (example 12589): $NC"
+   read -r TEXTID
    STATUSRESULT=$(curl -# https://textbelt.com/status/"$TEXTID")
-   
-   echo " "
-   echo -e "$BGreen Final Response (JSON): $NC"
+   echo -e "\n$BGreen Final Response (JSON): $NC"
    echo " "
    echo -e "$BGreen ---------------------------------------------- $NC"
    echo "$STATUSRESULT"
@@ -128,9 +107,7 @@ function SMSCHECK() {
 }
 
 function helpfunction() {
-   
-   echo "  "
-   echo -e "$BGreen Available opions: $NC"
+   echo -e "\n$BGreen Available opions: $NC"
    echo -e " "
    echo -e "$BGreen Show this page: --help $NC"
    echo -e "$BGreen Check text message status: --statuscheck $NC"
@@ -161,17 +138,15 @@ then
    
 elif [ "$1" == "--version" ]
    
-   then
-      VERSION
-
+then
+   VERSION
 
 elif [ $# -le 0 ]
 
 then
-   echo " "
-   echo -e "$Red No arguments specified! $NC"
-   echo -e "$Red Use$NC $BRed--help$NC $Red to display options.$NC"
-   echo " "
+   banner
+   SENDSMS
+   byemsg
 
 else
    echo " "
